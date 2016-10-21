b868f67c1fe634f6c24cbaad0b53bf9d518564e4
mbroersen <m.broersen@gmail.com>
2016-10-21 16:11:21 +0200

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+
+gulp.task('jsminify', function () {
+
+    return browserify({entries: ['./src/js/main.jsx'], extensions: ['.jsx'], debug: false, standalone: "SBJS"}).transform(babelify)
+        .bundle()
+        .pipe(source('sbjs.min.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js/'));
+
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./src/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./build/css'));
+});
+
+
+
+//gulp.task("watch", ['jsminify'], function () {
+//
+//});
+
